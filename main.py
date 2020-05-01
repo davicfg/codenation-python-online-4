7a330f3b289cabf0dfd9bf15c0c63054ebf724c8
davicfg <davicfg@hotmail.com>
2020-04-30 23:54:37 -0300

    Finalizando projeto

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import hashlib
 get = rh.get_json_cesar("1a6d008f1b4bd19cee0f1cb566dc8374a8f7020b")
 deciphered = Cesar().decipher(get["numero_casas"], get["cifrado"])
 get["decifrado"] = deciphered
-get["resumo_criptografico"] = hashlib.sha1(get["cifrado"].encode('utf-8')).hexdigest()
+get["resumo_criptografico"] = hashlib.sha1(get["decifrado"].encode('utf-8')).hexdigest()
 
 #save file
 f = open("answer.json", "w")
-f.write(str(get))
+f.write(str(get).replace("'", "\"").replace(",", ",\n "))
 f.close
 
 result = rh.post_file("1a6d008f1b4bd19cee0f1cb566dc8374a8f7020b")
